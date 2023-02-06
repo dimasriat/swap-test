58632a0151937b218b050863c2429d2f92625694
Dimas Riatmodjo <dimasriat@gmail.com>
2023-02-06 13:43:39 +0700

    feat: add User test example

--- test/ChainlinkSwap.t.sol
+++ test/ChainlinkSwap.t.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.13;
 import "lib/forge-std/src/Test.sol";
 import "src/ChainlinkSwap.sol";
 
+contract User {
+    ChainlinkSwap swap;
+    constructor(ChainlinkSwap _swap) {
+        swap = _swap;
+    }
+    function callMessage(string memory message) public {
+        swap.callMessage(message);
+    }
+}
+
 contract ChainlinkSwapTest is Test {
     ChainlinkSwap public swap;
 
@@ -13,5 +23,9 @@ contract ChainlinkSwapTest is Test {
 
     function testCall() public {
         swap.callMessage("hi bro");
+        
+        User alice = new User(swap);
+        alice.callMessage("alice was here");
+        console.log(address(alice));
     }
 }
