9e3cd43c667826fd88d7e3ed872fdf86b9d9df0e
Dimas Riatmodjo <dimasriat@gmail.com>
2023-02-07 14:47:17 +0700

    chore: add more constants

--- test/constants/Constants.sol
+++ test/constants/Constants.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+address constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+address constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+
 address constant ETH_USD_ORACLE = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
 address constant USDC_USD_ORACLE = 0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3;
