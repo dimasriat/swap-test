3c47e562b35e6f64c66df3de32ebe3d0ecbf9bad
Dimas Riatmodjo <dimasriat@gmail.com>
2023-02-07 08:04:14 +0700

    feat: create chainlinkswap

--- src/ChainlinkSwap.sol
+++ src/ChainlinkSwap.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract ChainlinkSwap {
-    event Call(address sender, string message);
+import "src/UniswapV3.sol";
 
-    function callMessage(string memory message) public {
-        emit Call(msg.sender, message);
+contract ChainlinkSwap {
+    UniswapV3 swap;
+    ChainlinkOracle oracle;
+    address WETH;
+
+    constructor(
+        UniswapV3 _swap,
+        ChainlinkOracle _oracle,
+        address _WETH
+    ) {
+        swap = _swap;
+        oracle = _oracle;
+        WETH = _WETH;
+    }
+
+    // TODO add slippage checker
+    function swapFromETH(address tokenOut) {
+        swap.swapFromETH(WETH, tokenOut, msg.value, 0);
+    }
+
+    // TODO add slippage checker
+    function swapToETH(address tokenIn, uint256 amountIn) {
+        swap.swapToETH(tokenIn, WETH, amountIn, 0);
     }
 }
